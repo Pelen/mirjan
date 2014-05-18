3f08d759bf979550de883ceb8f09d5acaed2c181
M P <ziomski@gmail.com>
2014-05-18 23:11:07 +0200

    added new tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -99,4 +99,43 @@ describe User do
 
   end
 
+  describe "user create" do  
+
+   it "adding user and increment user count" do
+       expect do
+          @user = FactoryGirl.create(:user)
+          @user.save
+      end.to change(User, :count).by(1)
+    end
+  end
+
+  describe "#POST #delete" do  
+  before :each do 
+      @user = FactoryGirl.create(:user) 
+      @user.save
+    end 
+  
+  it "remove user" do
+      expect do
+         User.delete(@user.id)
+      end.to change(User, :count).by(-1)
+    end
+  end
+
+describe "#POST #find" do  
+  before :each do 
+      @user = FactoryGirl.create(:user) 
+      @user.save
+    end 
+  
+  it "find user" do
+      expect do
+        @found_user = User.find(@user.id)
+        @found_user.id.should_receive(@user.id)
+    end
+end
+end
+
+
+
 end
